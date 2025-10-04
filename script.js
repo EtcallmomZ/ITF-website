3eeade1186550b3d2c09cbaf0ee80b35935c663a
eiweiw321 <eiweiw321@gmail.com>
2025-10-04 22:12:41 +0700

    commit -m "add a textarea and add a function"

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-const my_input = document.getElementById("my_input")
-let current_value = 1
+const account = document.getElementById("account")
+const cash = document.getElementById("cash")
+const result = document.getElementById("result")
+let current_account = 0
+let current_cash = 0
 
-function calc(x) {
-    current_value += Number(x)
-    my_input.vaule = current_value
+function calc(x,y) {
+    current_account += Number(x)
+    current_cash += Number(y)
+    const history_deploy =  `Current account balance : ${current_account}  , Current cash balance : ${current_cash}\n`
+    result.value += history_deploy
+}
+
+let 
+function money(x){
+
 }
